8cde57421609f6455d19d3cc117f7ad708dd3c17
mxxt1 <matt@dustcloudlabs.com>
2019-11-13 15:37:17 -0500

    server setup, dbconfig setup, index.js setup

---
+++ api/server.js
@@ -0,0 +1,15 @@
+//packages
+const express = require('express');
+
+
+
+//middleware
+const mainMW = require('../middleware/generalMW.js');
+
+//server
+
+const server = express();
+
+
+
+module.exports = server;
